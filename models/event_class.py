dd7cc58493896e8f3ca14516a4db13f5afe46214
mitkousa <uk.dimitar@gmail.com>
2021-08-05 13:58:55 +0100

    events created

---
+++ models/event_class.py
@@ -0,0 +1,11 @@
+class Event:
+
+    def __init__(self, date, event_name, number_of_guests, room_name, description):
+        self.date = date
+        self.event_name = event_name
+        self.number_of_guests = number_of_guests
+        self.room_name = room_name
+        self.description = description
+
+
+
---
+++ models/events.py
@@ -0,0 +1,7 @@
+from event_class import Event
+
+event1 = Event("05-08-2021", "wedding", 200, "Harris", "Wedding ceremony of John and Lara")
+event2 = Event("12-08-2021", "Birthday Party", 50, "Isla", "50th Birthday Party of Jack Jones")
+event3 = Event("14-09-2021", "christianing", 40, "Bute", "Christianing of baby Max")
+
+events = [event1, event2, event3]
